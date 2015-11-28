f057f8eed5a562dbea48fd4b1ad61d02e0e00ea2
Mujics <Mujics03@gmail.com>
2015-11-28 10:54:37 -0300

    Agrego lista de funcionabilidades a los roles

--- AerolineaFrba/Domain/Rol.cs
+++ AerolineaFrba/Domain/Rol.cs
@@ -13,12 +13,14 @@ namespace AerolineaFrba.Domain
         public int Cod_Rol { get; set; } 
         public string Nombre_Rol { get; set; }
         public bool Estado_Rol { get; set; }
+        public List<Funcionalidades> funcionalidades { get; set; }
         
-        public Rol( int Cod_Rol, string Nombre_Rol, bool Estado_Rol )
+        public Rol( int Cod_Rol, string Nombre_Rol, bool Estado_Rol, List<Funcionalidades> funcionalidades )
         {
             this.Cod_Rol = Cod_Rol;
             this.Nombre_Rol = Nombre_Rol;
             this.Estado_Rol = Estado_Rol;
+            this.funcionalidades = funcionalidades;
         }
     }
 }
