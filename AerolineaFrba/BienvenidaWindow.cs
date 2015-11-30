a2d9b922bffe5d2116c25cc960a5ff890fa35cad
Mujics <Mujics03@gmail.com>
2015-11-30 09:15:36 -0300

    Subo una pantalla de bienvenida

---
+++ AerolineaFrba/BienvenidaWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AerolineaFrba.Login;
+
+namespace AerolineaFrba
+{
+    public partial class BienvenidaWindow : Form
+    {
+        public BienvenidaWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form pantallaLogin = new LoginScreen();
+            this.Hide();
+            pantallaLogin.ShowDialog(this);
+            this.Close();
+        }
+        
+    }
+}
