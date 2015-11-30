85df298de235677a35fabf61f15c7bf7385265cc
Mujics <Mujics03@gmail.com>
2015-11-30 10:37:49 -0300

    error en archivo de inicio de aplicacion

--- AerolineaFrba/Program.cs
+++ AerolineaFrba/Program.cs
@@ -1,4 +1,4 @@
-pusing System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +16,7 @@ namespace AerolineaFrba
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BienvenidaWindow());
         }
     }
 }
