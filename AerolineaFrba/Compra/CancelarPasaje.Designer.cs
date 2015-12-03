d025f2449a1bfe47758798c968b7b1482ad9b28b
Mujics <Mujics03@gmail.com>
2015-12-03 16:06:52 -0300

    Avanzo con cancelar pasajes

--- AerolineaFrba/Compra/CancelarPasaje.Designer.cs
+++ AerolineaFrba/Compra/CancelarPasaje.Designer.cs
@@ -30,15 +30,19 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.fecha = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pasajesGrid = new System.Windows.Forms.DataGridView();
+            this.cancelar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.motivo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pasajesGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(139, 19);
+            this.label1.Location = new System.Drawing.Point(97, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 20);
             this.label1.TabIndex = 0;
@@ -47,51 +51,80 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Location = new System.Drawing.Point(12, 47);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 20);
+            this.label2.Size = new System.Drawing.Size(54, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "DNI";
+            this.label2.Text = "Fecha";
+            // 
+            // fecha
+            // 
+            this.fecha.Location = new System.Drawing.Point(85, 41);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(200, 26);
+            this.fecha.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pasajesGrid
+            // 
+            this.pasajesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pasajesGrid.Location = new System.Drawing.Point(12, 177);
+            this.pasajesGrid.Name = "pasajesGrid";
+            this.pasajesGrid.RowTemplate.Height = 28;
+            this.pasajesGrid.Size = new System.Drawing.Size(503, 150);
+            this.pasajesGrid.TabIndex = 7;
+            // 
+            // cancelar
+            // 
+            this.cancelar.Location = new System.Drawing.Point(12, 333);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(503, 30);
+            this.cancelar.TabIndex = 8;
+            this.cancelar.Text = "Cancelar Pasaje";
+            this.cancelar.UseVisualStyleBackColor = true;
+            this.cancelar.Click += new System.EventHandler(this.cancelar_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Location = new System.Drawing.Point(12, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Apellido";
+            this.label3.Size = new System.Drawing.Size(286, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Motivo ( cambiar por un textBox mejor ) ";
             // 
-            // label4
+            // motivo
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 175);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 231);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.motivo.Location = new System.Drawing.Point(304, 78);
+            this.motivo.Name = "motivo";
+            this.motivo.Size = new System.Drawing.Size(100, 26);
+            this.motivo.TabIndex = 10;
             // 
             // CancelarPasaje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 314);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(527, 375);
+            this.Controls.Add(this.motivo);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.cancelar);
+            this.Controls.Add(this.pasajesGrid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fecha);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "CancelarPasaje";
             this.Text = "CancelarPasaje";
+            ((System.ComponentModel.ISupportInitialize)(this.pasajesGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,8 +134,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker fecha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView pasajesGrid;
+        private System.Windows.Forms.Button cancelar;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox motivo;
     }
 }
--- AerolineaFrba/Compra/CancelarPasaje.cs
+++ AerolineaFrba/Compra/CancelarPasaje.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AerolineaFrba.Domain;
+using AerolineaFrba.Repositories;
 
 namespace AerolineaFrba.Compra
 {
@@ -15,6 +17,20 @@ namespace AerolineaFrba.Compra
         public CancelarPasaje()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // falta hacer find pasaje
+            //this.pasajesGrid.DataSource = new BindingSource(new BindingList<Pasaje>(new PasajesRepository().findPasaje()), null); 
+        }
+
+        private void cancelar_Click(object sender, EventArgs e)
+        {
+            var pasaje = (Pasaje)pasajesGrid.SelectedRows[0].DataBoundItem;
+            new PasajesRepository().cancelarPasaje( pasaje, fecha.Value, motivo.Text );
+            MessageBox.Show("Pasaje cancelado con exito");
+            //this.pasajesGrid.DataSource = new BindingSource(new BindingList<Pasaje>(new PasajesRepository().findPasaje()), null); 
         }
     }
 }
