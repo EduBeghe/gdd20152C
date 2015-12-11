f552ceb84151c2645239bfddf19c32dc7a90392c
Eduardo <eduardo@giro54.com>
2015-12-10 21:54:15 -0300

    Subo el cancelar

--- AerolineaFrba/Devolucion/RegistroDevolucion.cs
+++ AerolineaFrba/Devolucion/RegistroDevolucion.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AerolineaFrba.Repositories;
 
 namespace AerolineaFrba.Devolucion
 {
@@ -28,11 +29,38 @@ namespace AerolineaFrba.Devolucion
         {
             if (Validacion.validarInputs(this.Controls) &&
                 Validacion.soloNumeros(this.pnr, "PNR") &&
-                Validacion.soloNumeros(this.codigo, "Codigo"
+                Validacion.soloNumeros(this.codigo, "Codigo")
                 // && Validacion.fechaMayorMenos 
-                )) {  }
+                ) {
+                    var retorno = new DevolucionRepository().devolverCompra(DateTime.Today, int.Parse(pnr.Text), int.Parse(codigo.Text), motivo.Text);
+
+                if (retorno == 0)
+                {
+                    MessageBox.Show("Devolución realizada con éxito");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo realizar la devolución");
+                }
+            }
             
                 
+
+        }
+
+        private void RegistroDevolucion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fecha_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void motivo_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
