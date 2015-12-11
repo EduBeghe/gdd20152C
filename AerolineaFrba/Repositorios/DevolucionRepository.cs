4ddd062ae06795e7f24e77bf17cc3efa6f676ee5
Mujics <Mujics03@gmail.com>
2015-12-11 19:55:04 -0300

    Cambios a cancelar

--- AerolineaFrba/Repositorios/DevolucionRepository.cs
+++ AerolineaFrba/Repositorios/DevolucionRepository.cs
@@ -9,9 +9,9 @@ namespace AerolineaFrba.Repositories
 {
     class DevolucionRepository
     {
-       public int devolverCompra(DateTime fechaDevolucion,int numeroCompra ,int codigo ,String motivo){
+       public int devolverCompra( int numeroCompra ,int codigo ,String motivo, int tipo ){
             return DBAdapter.executeProcedureWithReturnValue("Cancelar_Pasajes_Encomiendas", 
-                fechaDevolucion,numeroCompra,codigo,motivo);
+                numeroCompra,codigo,motivo, tipo );
          } //fechaDevolucion ,@numeroCompra int,@codigo numeric(18),@motivo nvarchar(255))
     }
 }
